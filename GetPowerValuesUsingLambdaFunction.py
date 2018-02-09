f9a10fe45a1f18c69c1f6af83dbdddaecef26522
sammanasumary <mary@realimage.com>
2018-02-09 16:00:17 +0530

    initial commit

---
+++ GetPowerValuesUsingLambdaFunction.py
@@ -0,0 +1,10 @@
+"""z = lambda x,y:x+y
+print z(1,2)"""
+
+squares1=list(map(lambda x: x ** 2, [10,20,30,40]))
+print squares1
+
+numbers=[1,2,3,4]
+squares2=list(map(lambda x:pow(x,2), numbers))
+print squares2
+
